82c7e4bce2ecd9a11ce54df644f7398e117ad19e
Dean Lonergan <dlonerg1@jaguarlandrover.com>
2024-08-27 11:21:32 +0100

    Worked on client

--- src/service/service.cpp
+++ src/service/service.cpp
@@ -1,10 +1,14 @@
 #include <vsomeip/vsomeip.hpp>
 
+#define SERVICE_ID 0x1234
+#define INSTANCE_ID 0x5678
+
 std::shared_ptr< vsomeip::application > app;
 
 int main() {
 
     app = vsomeip::runtime::get()->create_application("World");
     app->init();
+    app->offer_service(SERVICE_ID, INSTANCE_ID);
     app->start();
 }
